2be55165dd2a976289e33d44b57abe8226923a82
Jonathan Wenger <jonathan.wenger@avalara.com>
2024-02-21 12:58:43 -0800

    Fix unit test and limit matrix for time being

--- spec/auth/oauth_helper_spec.rb
+++ spec/auth/oauth_helper_spec.rb
@@ -7,7 +7,7 @@ describe 'OAuthHelper' do
     @config = AvalaraSdk::Configuration.new
     @config.client_id = ENV["CLIENT_ID"]
     @config.client_secret = " "
-    @config.environment='qa'
+    @config.environment='production'
     @config.app_name="testApp"
     @config.app_version="2.3.1"
     @config.machine_name="AVL_WIN_007"
@@ -18,7 +18,7 @@ describe 'OAuthHelper' do
     it 'should be to initiate the device code flow' do
       begin
         device_auth_result = AvalaraSdk::Auth::OAuthHelper.initiate_device_authorization_flow("avatax_api", @config)
-        expect(device_auth_result.verification_url).to eq "https://ai-awsfqa.avlr.sh/device"
+        expect(device_auth_result.verification_url).to eq "https://identity.avalara.com/device"
         token_result = AvalaraSdk::Auth::OAuthHelper.get_access_token_for_device_flow(device_auth_result.device_code, @config)
         expect(token_result.error_message).to eq "authorization_pending"
         expect(token_result.http_status_code).to eq 400
